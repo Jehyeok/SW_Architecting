33fb5b99964a76c97a5cca211bb37dd45661471d
Jehyeok <hook3748@gmail.com>
2014-06-16 23:24:50 +0900

    gson 인터페이스 추출 전

---
+++ gson-2.2.4-sources/appClientModule/com/google/gson/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link com.google.gson.bind.Gson}.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.google.gson.annotations;
---
+++ gson-2.2.4-sources/appClientModule/com/google/gson/jehyeok/componentTypeAdapter.java
@@ -0,0 +1,7 @@
+package com.google.gson.jehyeok;
+
+import com.google.gson.type.TypeAdapter;
+
+public interface componentTypeAdapter {
+	public TypeAdapter<?> getAdepter();
+}
